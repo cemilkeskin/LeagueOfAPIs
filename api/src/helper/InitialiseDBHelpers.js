d18de61e0fe65a556fc8d536c178e13b984de168
Cemil Keskin <cemil.keskin@student.ehb.be>
2020-12-31 23:25:07 +0100

    feat(DBconnection): dbconnection fixed + records

--- api/src/helper/InitialiseDBHelpers.js
+++ api/src/helper/InitialiseDBHelpers.js
@@ -1,51 +1,51 @@
 
-const func = {
-  initialiseTables: async function (db) {
-    await db.schema.hasTable('champions').then(async (exists) => {
-      if (!exists) {
-        await db.schema
-          .createTable('champions', (table) => {
-            table.increments().primary();
-            table.string('uuid');
-            table.string('championName');
-            table.string('championKey');
-            table.string('role');
-            table.timestamps(true, true);
-          })
-          .then(async () => {
-            console.log('created table champions');
-          })
-          .catch((e) => {
-            // console.error(e)
-          })
-      }
+// const func = {
+//   initialiseTables: async function (db) {
+//     await db.schema.hasTable('champions').then(async (exists) => {
+//       if (!exists) {
+//         await db.schema
+//           .createTable('champions', (table) => {
+//             table.increments().primary();
+//             table.string('uuid');
+//             table.string('championName');
+//             table.string('championKey');
+//             table.string('role');
+//             table.timestamps(true, true);
+//           })
+//           .then(async () => {
+//             console.log('created table champions');
+//           })
+//           .catch((e) => {
+//             // console.error(e)
+//           })
+//       }
 
-    })
+//     })
 
 
-    await db.schema.hasTable('roles').then(async (exists) => {
-      if (!exists) {
-        await db.schema
-          .createTable('roles', (table) => {
-            table.increments().primary();
-            table.uuid('uuid');
-            table.string('marksman'); 
-            table.string('mages'); 
-            table.string('fighter');
-            table.string('tank');
-            table.string('assassin'); 
-            table.string('support');
-            table.timestamps(true, true);
-          })
-          .then(async () => {
-            console.log('created table lane');
-          })
-          .catch((e) => {
-            // console.error(e)
-          })
-      }
-    })
-  }
-}
+//     await db.schema.hasTable('roles').then(async (exists) => {
+//       if (!exists) {
+//         await db.schema
+//           .createTable('roles', (table) => {
+//             table.increments().primary();
+//             table.uuid('uuid');
+//             table.string('marksman'); 
+//             table.string('mages'); 
+//             table.string('fighter');
+//             table.string('tank');
+//             table.string('assassin'); 
+//             table.string('support');
+//             table.timestamps(true, true);
+//           })
+//           .then(async () => {
+//             console.log('created table lane');
+//           })
+//           .catch((e) => {
+//             // console.error(e)
+//           })
+//       }
+//     })
+//   }
+// }
 
-module.exports = func
+// module.exports = func
--- api/src/index.js
+++ api/src/index.js
@@ -4,13 +4,23 @@ const bodyParser = require('body-parser');
 const md5 = require('md5');
 const jwtToken = require('jsontokens')
 
- 
+
+
 const DatabaseHelper = require('./helper/DatabaseHelper');
 const InitialiseDBHelpers = require('./helper/InitialiseDBHelpers')
 const UUIDHelper = require('./helper/UuidHelpers');
+/*
 const AuthHelper = require('./helper/AuthHelper');
-
 InitialiseDBHelpers.initialiseTables(DatabaseHelper);
+*/
+
+const pg = require('knex')({
+  client: 'pg',
+  version: '9.6',
+  searchPath: ['knex', 'public'],
+  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+});
+
 
 app.use(bodyParser.json());
 app.use(
@@ -24,71 +34,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/login', async (req, res) => {
-  const uData = req.body;
-  if (uData && uData.password && uData.email) {
-    const regex = /^\S+@\S+\.\S+$/;
-    if (regex.test(uData.email)) {
-      const toMatch = {
-        email: uData.email,
-        password: md5(uData.password)
-      }
-      await DatabaseHelper.table('users').select(['uuid', 'username', 'email']).where(toMatch).then((data) => {
-        if (data.length == 0) {
-          res.status(401).send()
-        }
-        else {
-
-          jwt = new jwtToken.TokenSigner('ES256K', "private key").sign(data[0])
-          res.status(200).send(jwt)
-
-        }
-      })
-        .catch((e) => {
-          res.status(401).send(e)
-        })
-    }
-    else {
-      res.status(400).send()
-    }
-  }
-  else {
-    res.status(400).send()
-  }
-})
-
-
-
-app.post('/register', async (req, res) => {
-  const uData = req.body;
-  if (uData && uData.username && uData.password && uData.email) {
-    const regex = /^\S+@\S+\.\S+$/;
-    if (regex.test(uData.email)) {
-      // insert
-      const toInsert = {
-        uuid: UUIDHelper.generateUUID(),
-        username: uData.username,
-        email: uData.email,
-        password: md5(uData.password)
-      }
-      await DatabaseHelper.table('users').insert(toInsert).then((data) => {
-        res.status(200).send()
-      })
-        .catch((e) => {
-          res.status(401).send(e)
-        })
-    }
-    else {
-      res.status(400).send()
-    }
-  }
-  else {
-    res.status(400).send()
-  }
-})
-
-
-
 app.get('/join', async (req, res) => {
   await DatabaseHelper
     .table('items')
@@ -99,7 +44,7 @@ app.get('/join', async (req, res) => {
     })
 
 })
-
+/*
 app.get('/questions', AuthHelper.tokenValidator, async (req, res) => {
   await DatabaseHelper.table('records').select('*').where({ user_id: req.body.user.uuid }).then((data) => {
     res.send(data);
@@ -107,7 +52,9 @@ app.get('/questions', AuthHelper.tokenValidator, async (req, res) => {
     res.send(error).status(400)
   })
 })
+*/
 
+/*
 app.get('/question/:uuid', AuthHelper.tokenValidator, async (req, res) => {
   if (req.params.uuid) {
     await DatabaseHelper.table('records').select('*').where({ uuid: req.params.uuid }).then((data) => {
@@ -126,38 +73,11 @@ app.get('/question/:uuid', AuthHelper.tokenValidator, async (req, res) => {
     res.send(400)
   }
 })
-/**
-* 
+ 
 */
-app.post('/question', AuthHelper.tokenValidator, async (req, res) => {
-  const question = req.body.question;
-  const response = await ConversationHelpers.senseEmotionHelper(question)
-  const uuid = UUIDHelper.generateUUID();
-  if (response) {
-    const toInsertQuestion = {
-      uuid: uuid,
-      question: question,
-      answer: response.toString(),
-      user_id: req.body.user.uuid
-    }
-    await DatabaseHelper.insert(toInsertQuestion).table('records').returning('*').then(async (data) => {
-      if (response == null) {
-        res.sendStatus(402)
-      }
-      else {
-        const answer = { ...ConversationHelpers.convertEmotionValue(response), uuid: uuid };
-        res.send(answer);
-      }
-    }).catch((e) => {
-      res.status(401).send(e)
-    })
-  }
-  else {
-    res.status(400).send()
-  }
-})
 
 
+/*
 app.patch('/question/:uuid', AuthHelper.tokenValidator, async (req, res) => {
   if (req.params.uuid && req.body) {
     const toAlter = {};
@@ -196,9 +116,55 @@ app.delete('/question/:uuid', AuthHelper.tokenValidator, async (req, res) => {
     res.send(400)
   }
 })
+*/
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(process.env.PORT || 3001, () => console.log(`Listening on port ${process.env.PORT || 3001}`)); 
+  app.listen(process.env.PORT || 3001, () => console.log(`Listening on port ${process.env.PORT || 3001}`));
 }
+
+
+
+async function initialiseTables() {
+  await pg.schema.hasTable('champions').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('champions', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('championName');
+          table.string('championKey');
+          table.integer('role');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table champions');
+         
+        });
+
+    }
+  });
+  await pg.schema.hasTable('roles').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('roles', (table) => {
+          table.increments().primary();
+          table.uuid('uuid');
+          table.string('marksman');
+          table.string('mages');
+          table.string('fighter');
+          table.string('tank');
+          table.string('assassin');
+          table.string('support');
+          table.timestamps(true, true);
+        }) 
+        .then(async () => {
+          console.log('created table roles');
+          
+        });
+
+    }
+  });
+}
+initialiseTables()
 
 module.exports = app
